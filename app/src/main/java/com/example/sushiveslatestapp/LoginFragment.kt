97f95cf91b231134d2027c8affeaa1c868923076
Usvel <rodion.hairof228@yandex.ru>
2021-09-12 09:47:32 +0300

    Cверстал активити и фрагменты.

---
+++ app/src/main/java/com/example/sushiveslatestapp/LoginFragment.kt
@@ -0,0 +1,24 @@
+package com.example.sushiveslatestapp
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class LoginFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = LoginFragment()
+    }
+
+    private lateinit var viewModel: LoginViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.login_fragment, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/sushiveslatestapp/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.sushiveslatestapp
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
